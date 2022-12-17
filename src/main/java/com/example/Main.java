e269aa860e87641ee36119ffcbbaf6915df4c833
Parikshit Narang <parikshit3097@gmail.com>
2022-12-16 21:15:12 -0800

    added apis to fetch customer data from postgres

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,41 +1,45 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Objects;
 
-@ComponentScan(basePackages = "com.example")
-@EnableAutoConfiguration
-@Configuration
+@SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+
+    }
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/greet")
-    public greetResponse greet() {
-        return new greetResponse("Hello",
-                List.of("Java", "C#"),
-                new Person("Parikshit Narang", 25, Double.MIN_VALUE));
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
     }
 
-    record Person(String person, int age, double savings){
-
-    }
-
-    record greetResponse(
-            String greet,
-            List<String> favProgrammingLanguages,
-            Person person
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
     ){
 
+    }
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request){
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
     }
 }
